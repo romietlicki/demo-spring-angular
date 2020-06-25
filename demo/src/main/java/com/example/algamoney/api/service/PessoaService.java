0e8f7ae667251c370e9e4a91d0786a47ed262d1a
Rodrigo <rodrigo.mietlicki@gmail.com>
2020-06-25 17:13:54 -0300

    Adding atualizarPropriedadeAtivo method to update only the ativo property

--- demo/src/main/java/com/example/algamoney/api/service/PessoaService.java
+++ demo/src/main/java/com/example/algamoney/api/service/PessoaService.java
@@ -23,15 +23,27 @@ public class PessoaService {
 	private PessoaRepository pessoaRepository;
 
 	public Pessoa atualizar(Long codigo, Pessoa pessoa) {
-		Optional<Pessoa> pessoaSalva = pessoaRepository.findById(codigo);	
+		Optional<Pessoa> pessoaSalva = buscarPessoaPeloCodigo(codigo);
 		
-		if(pessoaSalva == null) {
-			throw new NoSuchElementException("not Found" );
-		}
 		BeanUtils.copyProperties(pessoa, pessoaSalva.get(), "codigo");
-	
 		return pessoaRepository.save(pessoaSalva.get());
-	
+
+	}
+
+
+	public void atualizarPropriedadeAtivo(Long codigo, Boolean ativo) {
+		
+		Optional<Pessoa> pessoaSalva = buscarPessoaPeloCodigo(codigo);
+			pessoaSalva.get().setAtivo(ativo);
+				pessoaRepository.save(pessoaSalva.get());
 	}
 	
+	private Optional<Pessoa> buscarPessoaPeloCodigo(Long codigo) {
+		Optional<Pessoa> pessoaSalva = pessoaRepository.findById(codigo);
+		
+		if (pessoaSalva == null) {
+			throw new NoSuchElementException("Not Found");
+		}
+		return pessoaSalva;
+	}
 }
